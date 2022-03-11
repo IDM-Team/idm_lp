a356f4ef17fe46e5d7acdeb42929e00edd9d5d31
lordralinc <46781434+lordralinc@users.noreply.github.com>
2022-03-12 01:41:50 +0300

    1.13.0

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,8 +1,15 @@
-sudo apt update
-sudo apt install git curl
-curl https://cli-assets.heroku.com/install.sh | sh
+echo "Обновление пакетов"
+sudo apt update > /dev/null
+
+echo "Установка git & curl"
+sudo apt install git curl> /dev/null
+
+echo "Установка Heroku CLI"
+curl https://cli-assets.heroku.com/install.sh | sh > /dev/null
+
 heroku login
 
+echo "Создание структуры проекта"
 mkdir idm_lp
 mkdir idm_lp/idm_lp
 cd idm_lp || exit
@@ -11,15 +18,17 @@ curl https://raw.githubusercontent.com/IDM-Team/idm_lp/heroku-deploy/Procfile > 
 curl https://raw.githubusercontent.com/IDM-Team/idm_lp/heroku-deploy/requirements.txt > requirements.txt
 curl https://raw.githubusercontent.com/IDM-Team/idm_lp/heroku-deploy/runtime.txt > runtime.txt
 
-echo Пожалуйста введите токен
+echo "Пожалуйста введите токен"
 read token
 
 echo '{"tokens": ["'$token'"]}' > idm_lp/config.json
 
-echo Пожалуйста введите имя приложения на Heroku
+echo "Пожалуйста введите имя приложения на Heroku"
 read name
-git init
-heroku git:remote -a $name
-git add .
-git commit -am "make it better"
+
+echo "Отправка файлов на сервер Heroku"
+git init > /dev/null
+heroku git:remote -a $name > /dev/null
+git add . > /dev/null
+git commit -am "make it better" > /dev/null
 git push heroku master
