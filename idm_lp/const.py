62a9a60733f7dc5b1c0a7360f36511cbc36fe400
lordralinc <46781434+lordralinc@users.noreply.github.com>
2022-03-12 23:25:54 +0300

    1.13.10

--- idm_lp/const.py
+++ idm_lp/const.py
@@ -2,7 +2,7 @@ import json
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-__version__ = '1.13.8'
+__version__ = '1.13.10'
 __author__ = 'lordralinc'
 __description__ = (
     "LP модуль позволяет работать приемнику сигналов «IDM multi» работать в любых чатах. "
